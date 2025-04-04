80cf2e8a71bdec74306421bcaf89133d2e35b1c0
Abdul4real <Info@alizafcare.ca>
2025-04-04 16:43:01 -0400

    Initial commit with class library

---
+++ StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace StringExtensionsLib
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            return char.IsUpper(str[0]);
+        }
+    }
+}
